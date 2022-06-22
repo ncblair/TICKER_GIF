ba73e0d7a2e3848c601ee70838e690972d7225d2
Nathan Blair <nathan@mat.ucsb.edu>
2022-06-22 16:14:06 -0700

    Add ticker website via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+let ftext = "";
+let fwidth = 0;
+let fheight = 0;
+let ffontsize = 0;
+let ffontcolor = "#000000";
+let fbackgroundcolor = "#ffffff";
+let fduration = 1;
+let ftype = "Arial";
+
+function startSketch(){
+    let sketch = function( p ) {
+        ftext = document.getElementById("ftext").value + " ";
+        fwidth = Number(document.getElementById("fwidth").value);
+        fheight = Number(document.getElementById("fheight").value);
+        ffontsize = Number(document.getElementById("ffontsize").value);
+        ffontcolor = document.getElementById("ffontcolor").value;
+        fbackgroundcolor = document.getElementById("fbackgroundcolor").value;
+        fduration = Number(document.getElementById("fduration").value);
+        if (document.getElementById("ftype").value != '') {
+            ftype = p.loadFont(document.getElementById("ftype").value);
+        }
+       
+
+        p.setup = function() {
+            let canv = p.createCanvas(fwidth, fheight);
+            p.frameRate(30);
+            p.createLoop({duration:fduration, gif:true});
+            canv.hide();
+        };
+
+        p.draw = function() {
+            p.background(fbackgroundcolor);
+            p.fill(ffontcolor);
+            p.translate(p.width / 2, p.height / 2);
+            p.textSize(ffontsize);
+            p.textFont(ftype);
+
+            let text_width = p.textWidth(ftext);
+            if (text_width <= 0.1) {
+                text_width = 0.1;
+            }
+
+            const x = p.animLoop.theta * text_width / (2 * Math.PI) - p.width / 2 - text_width;
+            let num_repetitions = Math.ceil(p.width / text_width) + 1;
+            ftext_repeated = ftext.repeat(num_repetitions);
+            p.text(ftext_repeated, x, p.textAscent(ftext) / 2);
+        };
+    };
+ 
+    let myp5 = new p5(sketch);
+ }
+
+
+// var submitted = false;
+// function setup() {
+//     // createLoop({duration:0.6, gif:true});
+// }
+
+// function draw() {
+//     if (submitted) {
+        
+//     }
+//     // ellipse(x, y, 50, 50);
+// }
+
+// function make_gif() {
+//     createCanvas(800, 100);
+//     fill(0);
+//     frameRate(30);
+//     createLoop({duration:0.6, gif:true});
+//     submitted = true;
+// }
