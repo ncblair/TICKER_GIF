6f3a2bd681af910d59ffd498566b42b8db5155a9
Ncblair <nblair@berkeley.edu>
2022-06-22 18:00:42 -0700

    actually fixed vertical scaling

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function startSketch(){
             let canv = p.createCanvas(fwidth, fheight);
             canv.hide();
             p.frameRate(30);
-            p.createLoop({duration:fduration, gif:true});
+            p.createLoop({duration:Math.abs(fduration), gif:true});
         };
 
         p.draw = function() {
@@ -37,17 +37,30 @@ function startSketch(){
             p.translate(p.width / 2, p.height / 2);
             p.textSize(ffontsize);
             p.textFont(ftype);
-            p.textAlign(p.LEFT, p.CENTER);
+            p.textAlign(p.LEFT, p.BOTTOM);
 
             let text_width = p.textWidth(ftext);
             if (text_width <= 0.1) {
                 text_width = 0.1;
             }
-
-            const x = p.animLoop.theta * text_width / (2 * Math.PI) - p.width / 2 - text_width;
+            let x = 0;
+            if (fduration > 0) {
+                x = -p.animLoop.theta * text_width / (2 * Math.PI) - p.width / 2;
+            }
+            else {
+                x = p.animLoop.theta * text_width / (2 * Math.PI) - p.width / 2 - text_width;
+            }
+            
             let num_repetitions = Math.ceil(p.width / text_width) + 1;
             ftext_repeated = ftext.repeat(num_repetitions);
-            p.text(ftext_repeated, x, -p.textDescent(ftext) / 2);
+            let text_height = p.textAscent(ftext) - p.textDescent(ftext);
+
+            p.text(ftext_repeated, x, text_height / 2 + p.textDescent(ftext));
+            // p.fill("red");
+            // p.stroke("red");
+            // p.line(-p.width / 2, 0, p.width/2, 0);
+            // p.line(0, 0, 0, p.textAscent(ftext));
+            // p.line(0, 0, 0, -p.textDescent(ftext));
         };
     };
  
